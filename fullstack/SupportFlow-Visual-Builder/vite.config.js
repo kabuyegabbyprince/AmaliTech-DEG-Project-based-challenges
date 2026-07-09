0a9072612fb8d4976cb41f4b90a3f5e007bce300
prince <126168873+kabuyegabbyprince@users.noreply.github.com>
2026-07-09 16:05:53 +0200

    Update vite.config.js

--- fullstack/SupportFlow-Visual-Builder/vite.config.js
+++ fullstack/SupportFlow-Visual-Builder/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AmaliTech-DEG-Project-based-challenges/',
+  base: '/AmaliTech-DEG-Project-based-challenges/fullstack/SupportFlow-Visual-Builder/',
 })
