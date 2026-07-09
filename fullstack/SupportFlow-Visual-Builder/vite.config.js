cc6a8ad0e6aea9f764d0b2ac673646afac372f99
prince <126168873+kabuyegabbyprince@users.noreply.github.com>
2026-07-09 17:14:27 +0200

    Replace React plugin with Vue plugin in Vite config

--- fullstack/SupportFlow-Visual-Builder/vite.config.js
+++ fullstack/SupportFlow-Visual-Builder/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/AmaliTech-DEG-Project-based-challenges/fullstack/SupportFlow-Visual-Builder/',
+  plugins: [vue()],
+  base: '/AmaliTech-DEG-Project-based-challenges/fullstack/SupportFlow-Visual-Builder/'
 })
