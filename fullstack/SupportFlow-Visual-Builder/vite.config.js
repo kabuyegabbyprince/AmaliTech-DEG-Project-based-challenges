ee7d83e6ac0360de965d271739aedf62ef783b13
prince <126168873+kabuyegabbyprince@users.noreply.github.com>
2026-07-09 17:21:48 +0200

    correct file

--- fullstack/SupportFlow-Visual-Builder/vite.config.js
+++ fullstack/SupportFlow-Visual-Builder/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
   base: '/AmaliTech-DEG-Project-based-challenges/fullstack/SupportFlow-Visual-Builder/'
 })
